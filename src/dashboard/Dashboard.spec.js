b2e4545c43062c68b401d436fd29caa690bb084a
Shafi Masoumi <masoumi2017@icloud.com>
2019-12-05 20:28:21 -0800

    add some test in Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+
+import React from 'react';
+import { render } from "react-testing-library";
+import Dashboard from './Dashboard';
+
+test("Dashboard shows the controls and display", () => {
+  const { getByText } = render(<Dashboard />);
+
+  getByText(/Unlocked/i);
+  const display = getByText(/Open/i);
+  getByText(/Lock Gate/i);
+  const controls = getByText(/Close Gate/i);
+
+  expect(display).toBeDefined;
+  expect(controls).toBeDefined;
+}); 
+
